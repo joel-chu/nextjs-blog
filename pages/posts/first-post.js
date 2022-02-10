6efe51c6923a8c41f206c71d4f0a0efce48ef5d7
Joel Chu <joelckchu@gmail.com>
2022-02-10 20:39:39 +0800

    lessons

---
+++ pages/posts/first-post.js
@@ -0,0 +1,32 @@
+import Link from 'next/link'
+import Head from 'next/head'
+
+/*
+import Script from 'next/script'
+<Script
+  src="https://connect.facebook.net/en_US/sdk.js"
+  strategy="lazyOnload"
+  onLoad={() =>
+    console.log(`script loaded correctly, window.FB has been populated`)
+  }
+/>
+*/
+
+import Layout from '../../components/layout'
+
+export default function FirstPost() {
+  return (
+    <Layout>
+      <Head>
+        <title>First Post</title>
+      </Head>
+
+      <h1>First Post</h1>
+      <h2>
+        <Link href="/">
+          <a>Back to home</a>
+        </Link>
+      </h2>
+    </Layout>
+  )
+}
